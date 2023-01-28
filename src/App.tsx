720254aa3ba2b2c721baca5902b62fd44a2d49cf
Ruairi Blake <rblake0601@gmail.com>
2023-01-28 16:00:19 -0500

    sets up react-router-dom with blank pages for home aand about

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
+import Home from './Pages/Home/home';
+import About from './Pages/About/about'
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          {/* <Route path="/example" element={<examplePage/>}/> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/Pages/Home/home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Home = () => {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        WIP - RPGToolChest main page
+      </header>
+    </div>
+  )
+}
+
+export default Home;
